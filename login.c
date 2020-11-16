c624f848b2289f9f8c50c054cf3e07e26e78b594
Adriano Holanda <aholanda@usp.br>
2020-11-16 18:32:29 -0300

    add getch

--- login.c
+++ login.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <termios.h>
+#include <unistd.h>
 
 #include "login.h"
+#include "password.h"
 
 static void flush_buffer() {
     char ch;
     while ((ch = getchar() != '\n') && (ch != EOF));
 }
 
+int getch() {
+    struct termios oldtc, newtc;
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldtc);
+    newtc = oldtc;
+    newtc.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newtc);
+    ch=getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldtc);
+    return ch;
+}
 
 static void __registrar_usuario(Usuario *usuario) {
     FILE *fp;
@@ -24,38 +38,40 @@ static void __registrar_usuario(Usuario *usuario) {
 }
 
 void registrar_usuario(Usuario *usuario) {
-    char c='\0';
-    int continua=1;
-    int i, pos=0;
+    char senha[MAXPW] = {0};
+    char ch = '\0';
+    int i=0;
 
     printf("\n\n\t  >> Registrar usuario:\n");
     printf("\n\n\t  >> Login:");
     scanf("%s", usuario->login);
-    
     flush_buffer();
-    
-    while (continua) {
-      fflush(stdin);
-      printf("\n\t  >> Senha:");
-      for (i=0; i<pos; i++)
-        printf("*"); 
-      c=fgetc(stdin);
-      usuario->senha[pos++]=c;
-      if (c == '\n') {
-        continua = 0;
-        usuario->senha[pos] = '\0';
-      } 
-    } 
+
+    printf("\n\n\t Digite a senha e");
+    printf("tecle Enter para sair.\n\n");
+
+    for (;;) {
+        ch = getch();
+        senha[i++] = ch;
+        printf("*");
+        if(ch == '\n') {
+            i--;
+            senha[i] = '\0';
+            break;
+        }
+    }
   
-    printf("\n%s\n",usuario->senha);
+    printf("\n%s\n", senha);
+
+    strncpy(usuario->senha, senha, MAXPW);
 
     __registrar_usuario(usuario);
 }
 
 int checar_usuario() {
     FILE *fp;
-    char login[TAMLOGIN];
-    char senha[TAMSENHA];
+    char login[MAXLOGIN];
+    char senha[MAXPW];
     Usuario usuario;
   
     printf("Autenticar usuario:\n");
@@ -73,8 +89,8 @@ int checar_usuario() {
 
     while (!feof(fp)) {
         fread(&usuario, sizeof(Usuario), 1, fp);
-        if (strncmp(&usuario.login[0], login, TAMLOGIN) == 0) {
-            if (strncmp(&usuario.senha[0], senha, TAMSENHA) == 0) {
+        if (strncmp(&usuario.login[0], login, MAXLOGIN) == 0) {
+            if (strncmp(&usuario.senha[0], senha, MAXPW) == 0) {
                 return 1;
             }
         }
