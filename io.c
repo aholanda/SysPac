bd48d8ad5701c1e81b556bf5b51a04b7e4f31c57
Adriano J. Holanda <ajholanda@gmail.com>
2020-11-19 17:17:39 -0300

    io funcs

---
+++ io.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+
+void flush_buffer() {
+    char ch;
+    while ((ch = getchar() != '\n') && (ch != EOF));
+}
+
+int getch() {
+    struct termios oldtc, newtc;
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldtc);
+    newtc = oldtc;
+    newtc.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newtc);
+    ch=getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldtc);
+    return ch;
+}
---
+++ io.h
@@ -0,0 +1,7 @@
+#ifndef IO_H_INCLUDED
+#define IO_H_INCLUDED
+
+extern void flush_buffer(void);
+extern int getch(void);
+
+#endif
